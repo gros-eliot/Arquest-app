1ba3066f9b46e2a494ae53512aed61dc460e0d80
gros-eliot <eliot.gros@edu.univ-fcomte.fr>
2022-05-13 10:59:56 +0200

    couleurs gds

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import GuideStyle from '../views/GuideStyle.vue'
 import QuestCreateView from '../views/QuestCreateView.vue'
 import HistoryView from '../views/HistoryView.vue'
 import AgendaView from '../views/AgendaView.vue'
 import AvatarView from '../views/AvatarView.vue'
 import GiftsView from '../views/GiftsView.vue'
+
+import GuideStyle from '../views/GuideStyle.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,11 +16,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/gds',
-      name: 'GuideStyle',
-      component: GuideStyle,
-    },
+
     {
       path: '/create_quest',
       name: 'QuestCreateView',
@@ -44,6 +41,12 @@ const router = createRouter({
       path: '/gifts',
       name: 'GiftsView',
       component: GiftsView,
+    },
+
+    {
+      path: '/gds',
+      name: 'GuideStyle',
+      component: GuideStyle,
     },
   ]
 })
--- src/views/GuideStyle.vue
+++ src/views/GuideStyle.vue
@@ -1,22 +1,19 @@
-<script>
-</script>
-
 <template>
   <div class="">
     <h1 class="">Guide de style | Arquest</h1>
     <img src="" alt="" /><!--METTRE LOGO ARQUEST-->
   </div>
 
-  <div class="flex flex-col">
-    <div class="flex flex-row gap-10">
-      <span class="bg-red-500 w-30 h-6"></span
-      ><span class="bg-indigo-500 w-30 h-6"></span
-      ><span class="bg-indigo-300 w-30 h-6"></span
-      ><span class="bg-gray-extended-700 w-30 h-6"></span
-      ><span class="bg-gray-900 w-30 h-6"></span>
+  <div class="flex flex-col m-2">
+    <div class="flex flex-row justify-center gap-5 p-3">
+      <span class="bg-red-500 w-20 h-10"></span>
+      <span class="bg-indigo-500 w-20 h-10"></span>
+      <span class="bg-indigo-300 w-20 h-10"></span>
+      <span class="bg-gray-extended-300 w-20 h-10"></span>
+      <span class="bg-gray-900 w-20 h-10"></span>
     </div>
 
-    <div class="flex flex-row gap-0">
+    <div class="flex flex-row justify-center gap-6 p-3">
       <span class="bg-lime-400 w-10 h-5"></span>
       <span class="bg-cyan-200 w-10 h-5"></span>
       <span class="bg-red-400 w-10 h-5"></span>
@@ -27,7 +24,16 @@
       <span class="bg-yellow-400 w-10 h-5"></span>
     </div>
   </div>
+
+  <div>
+    
+  </div>
 </template>
+
+
+<script>
+</script>
+
 
 <style>
 </style>
