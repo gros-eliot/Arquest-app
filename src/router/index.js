130c9dd1c8396920d47f4d67608326d9372bb28a
gros-eliot <eliot.gros@edu.univ-fcomte.fr>
2022-05-14 21:03:53 +0200

    placeholder

--- src/router/index.js
+++ src/router/index.js
@@ -5,60 +5,24 @@ import HistoryView from '../views/HistoryView.vue'
 import AgendaView from '../views/AgendaView.vue'
 import AvatarView from '../views/AvatarView.vue'
 import GiftsView from '../views/GiftsView.vue'
-import ContactView from '../views/ContactView.vue'
 
+import ContactView from '../views/ContactView.vue'
+import MentionslegalesView from '../views/MentionslegalesView.vue'
 import GuideStyle from '../views/GuideStyle.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/create_quest', name: 'QuestCreateView', component: QuestCreateView, },
+    { path: '/history', name: 'HistoryView', component: HistoryView, },
+    { path: '/agenda', name: 'AgendaView', component: AgendaView, },
+    { path: '/avatar', name: 'AvatarView', component: AvatarView, },
+    { path: '/gifts', name: 'GiftsView', component: GiftsView, },
 
-    {
-      path: '/create_quest',
-      name: 'QuestCreateView',
-      component: QuestCreateView,
-    },
-    {
-      path: '/history',
-      name: 'HistoryView',
-      component: HistoryView,
-    },
-    {
-      path: '/agenda',
-      name: 'AgendaView',
-      component: AgendaView,
-    },
-    {
-      path: '/avatar',
-      name: 'AvatarView',
-      component: AvatarView,
-    },
-    {
-      path: '/gifts',
-      name: 'GiftsView',
-      component: GiftsView,
-    },
-
-    {
-      path: '/gds',
-      name: 'GuideStyle',
-      component: GuideStyle,
-    },
-    {
-      path: '/contact',
-      name: 'ContactView',
-      component: ContactView,
-    },
-    {
-      path: '/mentionslegales',
-      name: 'MentionslegalesView',
-      component: MentionslegalesView,
-    },
+    { path: '/contact', name: 'ContactView', component: ContactView, },
+    { path: '/mentionslegales', name: 'MentionslegalesView', component: MentionslegalesView, },
+    { path: '/gds', name: 'GuideStyle', component: GuideStyle },
   ]
 })
 
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,56 +1,126 @@
 <template>
+  <div class="bg-black ...">
+    <h2 class="text-4xl text-left ml-10 text-indigo-500 weight-700">
+      Nous contacter
+    </h2>
+    <p class="text-1xl text-right mr-9 h-40 text-white italic">
+      Vous avez une question ? Besoin d’un renseignement ? Tout ce passe ici.
+    </p>
 
-    <div class="bg-black ...">
+    <p class="text-2xl text-left ml-10 text-white uppercase">Nom</p>
+    <div>
+      <input
+        name="first_name"
+        id="first_name"
+        type="text"
+        placeholder="Insérez du texte"
+        class="text-2xl italic"
+        required
+      />
+    </div>
 
-        <h2 class="text-4xl text-left ml-10 text-indigo-500 weight-700">Nous contacter</h2>
-        <p class="text-1xl text-right mr-9 h-40 text-white italic">Vous avez une question ?  Besoin d’un renseignement ?  Tout ce passe ici.</p>
+    <p class="text-2xl text-left ml-10 text-white uppercase">Prénom</p>
+    <div>
+      <label class="text-2xl italic">Insérez du texte</label>
+      <input name="last_name" id="last_name" type="text" required />
+    </div>
 
-        <p class="text-2xl text-left ml-10 text-white">NOM</p>
-        <div>
-            <label class="text-2xl italic">Insérez du texte</label>
-            <input name="first_name" id="first_name" type="text" required>
-        </div>
+    <p class="text-2xl text-left ml-10 text-white uppercase">Email</p>
+    <div>
+      <label class="text-2xl italic">Ex : exemple@exemple.fr</label>
+      <input name="email" id="email" type="text" required />
+    </div>
 
-        <p class="text-2xl text-left ml-10 text-white">PRENOM</p>
-        <div>
-            <label class="text-2xl italic">Insérez du texte</label>
-            <input name="last_name" id="last_name" type="text" required>
-        </div>
+    <p class="text-2xl text-left ml-10 text-white uppercase">Téléphone</p>
+    <p class="text-1xl text-left ml-11 text-white italic">* facultatif</p>
+    <div>
+      <label class="text-2xl italic">Ex : 06 00 00 00 00</label>
+      <input name="telephone" id="telephone" type="text" required />
+    </div>
 
-        <p class="text-2xl text-left ml-10 text-white">EMAIL</p>
-        <div>
-            <label class="text-2xl italic">Ex : exemple@exemple.fr</label>
-            <input name="email" id="email" type="text" required>
-        </div>
-
-        <p class="text-2xl text-left ml-10 text-white">TELEPHONE</p>
-        <p class="text-1xl text-left ml-11 text-white italic">* facultatif</p>
-        <div>
-            <label class="text-2xl italic">Ex : 06 00 00 00 00</label>
-            <input name="telephone" id="telephone" type="text" required>
-        </div>
-        
-        <p class="text-2xl text-left ml-10 text-white">REQUETE</p>
-        <div>
-            <label class="bg-blue text-2xl italic">Insérez du texte</label>
-            <input name="email" id="email" type="text" required>
-        </div>
+    <p class="text-2xl text-left ml-10 text-white uppercase">Requête</p>
+    <div>
+      <label class="bg-blue text-2xl italic">Insérez du texte</label>
+      <input name="email" id="email" type="text" required />
+    </div>
 
     <div class="form-check">
-        <input class="form-check-input appearance-none ml-10 mt-20 h-4 w-4 border border-gray-300 squared-sm bg-white checked:bg-indigo-500 weight-700 checked:bg-indigo-500 weight-700 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label inline-block mt-20 text-white" for="flexCheckDefault">
-            En cochant cette case, j’accepte de recevoir les newsletters d’Arquest dans ma boite e-mail
-        </label>
-        </div>
-        
-        <div class="form-check">
-        <input class="form-check-input appearance-none ml-10 h-4 w-4 border border-gray-300 squared-sm bg-white checked:bg-indigo-500 weight-700 checked:bg-indigo-500 weight-700 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label inline-block mb-28 text-white" for="flexCheckDefault">
-            J’ai lu et j’autorise les conditions générales de vente Arquest. Pour plus d’informations  : conditions générales de vente
-        </label>
-        </div>
-     
+      <input
+        class="
+          form-check-input
+          appearance-none
+          ml-10
+          mt-20
+          h-4
+          w-4
+          border border-gray-300
+          squared-sm
+          bg-white
+          checked:bg-indigo-500
+          weight-700
+          checked:bg-indigo-500
+          weight-700
+          focus:outline-none
+          transition
+          duration-200
+          mt-1
+          align-top
+          bg-no-repeat bg-center bg-contain
+          float-left
+          mr-2
+          cursor-pointer
+        "
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+      <label
+        class="form-check-label inline-block mt-20 text-white"
+        for="flexCheckDefault"
+      >
+        En cochant cette case, j’accepte de recevoir les newsletters d’Arquest
+        dans ma boite e-mail
+      </label>
     </div>
+
+    <div class="form-check">
+      <input
+        class="
+          form-check-input
+          appearance-none
+          ml-10
+          h-4
+          w-4
+          border border-gray-300
+          squared-sm
+          bg-white
+          checked:bg-indigo-500
+          weight-700
+          checked:bg-indigo-500
+          weight-700
+          focus:outline-none
+          transition
+          duration-200
+          mt-1
+          align-top
+          bg-no-repeat bg-center bg-contain
+          float-left
+          mr-2
+          cursor-pointer
+        "
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+      <label
+        class="form-check-label inline-block mb-28 text-white"
+        for="flexCheckDefault"
+      >
+        J’ai lu et j’autorise les conditions générales de vente Arquest. Pour
+        plus d’informations : conditions générales de vente
+      </label>
+    </div>
+  </div>
 </template>
 
 
