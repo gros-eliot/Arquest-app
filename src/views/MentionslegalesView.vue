d735e63ac46c3657ebea7a8be988643d24cd6840
Helenajaegle <94958481+Helenajaegle@users.noreply.github.com>
2022-05-14 20:56:20 +0200

    contenu mentions légales

---
+++ src/views/MentionslegalesView.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <h2 class="text-2xl font-roboto font-bold">Mentions légales</h2>
+
+    <p class="text-base font-roboto">Cette page décrit les mentions légales qui vont s’imposer à tous les utilisateurs visitants ce site. En y naviguant, vous vous acquittez à les respecter. A prendre en compte que les mentions légales peuvent être changées à tout instant, c’est pour cela que nous vous convions à examiner assidûment. </p>
+
+    <h2 class="text-2xl font-roboto font-bold">Responsables  éditorial & légal</h2>
+  
+    <p class="text-base font-roboto">Nom de la strucutre - Arquest </p>
+    <p class="text-base font-roboto"> Représentée par - Matthieu Hoareau, Eliot Gros, Héléna Jaegle </p>
+    <p class="text-base font-roboto"> E-mail - Arquest@gmail.com </p>
+
+    <h2 class="text-2xl font-roboto font-bold"> Editeur du site</h2>
+
+    <p class="text-base font-roboto"> IUT Belfort-Montbéliard campus de Montbéliard BUT Métiers du Multimédia et de l’internet 19 Av. du Maréchal Juin, 90016 Belfort </p>
+    <p class="text-base font-roboto"> Représenté par les étudiants de première année - JAEGLE Héléna - HOAREAU Matthieu - GROS Eliot </p>
+    <p class="text-base font-roboto"> David@malsot.com Tuteur  légal des étudiants - David Malsot</p>
+
+    <h2 class="text-2xl font-roboto font-bold"> Hébergeur </h2>
+
+    <p class="text-base font-roboto"> Nom de la structure - Infomaniak & Netlify </p>
+    <p class="text-base font-roboto"> Representée par - Chris McCraw & Boris Siegenthaler </p>
+    <p class="text-base font-roboto"> Adresse Infomaniak - Rue Eugène-Marziano 25 1227 Les Acacias Netlify - 2325 3rd Street, Suite 296, San Francisco, California 94107. </p>
+
+    <h2 class="text-2xl font-roboto font-bold"> Conditions générales de vente </h2>
+
+    <p class="text-base font-roboto"> Le site est la propriété de l’IUT de Montbéliard en sa totalité, ainsi que l'ensemble des droits le concernant. Toute reproduction, intégrale ou partielle, est systématiquement soumise a l'autorisation des propriétaires. Toutefois, les liaisons du type hypertextes vers le site sont autorisées sans demandes spécifiques.</p>
+    <p class="text-base font-roboto"> Les utilisateurs reconnaîssent avoir pris connaissance, au moment de la passation au service premium, des conditions particulières de vente énoncées sur cet écran et déclare expressément les accepter sans réserve. Les conditions générales de vente régissent les relations contractuelles entre Arquest et ses utilisateurs, les deux parties les acceptant sans réserve. Ces conditions générales de vente prévaudront sur toutes autres conditions figurant dans tout autre document, sauf dérogation préalable, expresse et écrite. </p>
+
+
+
+
+
+</template>
+
+
+
+
+
+
+
+<script>
+export default {
+  name: "ContactView",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
