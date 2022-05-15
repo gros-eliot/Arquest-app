1a4abe935a653c358ea84c28d2360fecd5ecffd1
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-05-15 12:27:50 +0200

    Création de quête 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+<script>
+import QuestView from "../components/layouts/QuestView.vue";
+
+import HeaderView from "../components/layouts/HeaderView.vue";
+import NavView from "../components/layouts/NavView.vue";
+import FooterView from "../components/layouts/FooterView.vue";
+export default {
+  name: "Home",
+  components: {
+    QuestView,
+    HeaderView,
+    NavView,
+    FooterView,
+  },
+};
 </script>
 
+
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="bg-black text-white">
+    <h2>QUÊTES DU JOUR</h2>
+    <quest-view titre="Tarpon Bay" />
+  </div>
 </template>
