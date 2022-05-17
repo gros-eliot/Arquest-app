8dd1e8231ca12f5ad38ab11cdf82cbbe73b0f0e1
Helenajaegle <94958481+Helenajaegle@users.noreply.github.com>
2022-05-17 18:22:06 +0200

    ronds

--- src/components/Overlay/BravopartageView.vue
+++ src/components/Overlay/BravopartageView.vue
@@ -4,27 +4,13 @@
        <div class="overlay bg-black rounded-xl border-4 border-indigo-500 px-5 py-6">
            <h1 class="text-2xl font-press-start-2p text-center text-white">SPORT</h1>
             <h2 class="text-2xl font-roboto font-bold text-center mt-4 text-red-400">2 QUÊTES ACCOMPLIES !</h2>
-                
-            <div class="flex justify-center mt-4">
-                <img src="../../../src/assets/trophy.webp" alt="Logo trophy">
+            <p class="text-1xl font-roboto text-left mt-1 text-white">Partager</p>
+
+            <div class="rounded-full w-12 h-12 mt-4 bg-red-500"></div>
+            <h1 class="text-1xl font-roboto text-center text-white">Tom</h1>
+
             </div>
-              
-              <div
-            class="
-            bg-indigo-500
-            text-white
-            font-medium text-1xl 
-            w-5/6
-            py-3
-            m-6
-            uppercase
-            text-center
-            no-underline
-            rounded-xl
-            justify-center
-            active:bg-indigo-900 active:text-black active:border-black
-            hover:bg-indigo-900
-            "
-        >PARTAGER
-            <slot />
         </div>
+    </div>
+</template>
+
