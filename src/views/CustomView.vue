8ff0ef6f9b3c124b174d0208fb5ab1e3f7bf07d0
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-05-24 15:08:21 +0200

    modif custom

--- src/views/CustomView.vue
+++ src/views/CustomView.vue
@@ -4,25 +4,26 @@
     <div class="mt-96 pb-5">Insérer avatar</div>
   </div>
   <div class="bg-black">
-    <div class="grid grid-cols-3 text-center space-x-2 mx-2">
-      <div class="bg-indigo-300 rounded-xl py-2">Habits</div>
-      <div class="bg-indigo-300 rounded-xl py-2">Visage</div>
-      <div class="bg-indigo-300 rounded-xl py-2">Accessoires</div>
+    <div class="mx-2 grid grid-cols-3 space-x-2 text-center">
+      <div class="rounded-xl bg-indigo-300 py-2">Habits</div>
+      <div class="rounded-xl bg-indigo-300 py-2">Visage</div>
+      <div class="rounded-xl bg-indigo-300 py-2">Accessoires</div>
     </div>
     <div>
-      <div
-        class="grid grid-cols-4 text-center mt-10 gap-4 mx-11 pb-5 text-white"
-      >
-        <div class="border-2 rounded-md border-indigo-300">01</div>
-        <div class="border-2 rounded-md border-indigo-300">02</div>
-        <div class="border-2 rounded-md border-indigo-300">03</div>
-        <div class="border-2 rounded-md border-indigo-300">04</div>
-        <div class="border-2 rounded-md border-indigo-300">05</div>
-        <div class="border-2 rounded-md border-indigo-300">06</div>
-        <div class="border-2 rounded-md border-indigo-300">07</div>
-        <div class="border-2 rounded-md border-indigo-300">08</div>
-        <div class="border-2 rounded-md border-indigo-300">09</div>
+      <div class="mx-11 mt-10 grid grid-cols-4 gap-4 pb-5 text-center text-white">
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
+        <div class="rounded-md border-2 border-indigo-300"></div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+</script>
