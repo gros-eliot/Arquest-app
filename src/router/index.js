962ba1561f2a74f0fcc069206f9c482c91063d8a
gros-eliot <eliot.gros@edu.univ-fcomte.fr>
2022-06-09 22:33:40 +0200

    guard fonctionnel : si user pas connecté, aucun acces autres pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+//
+//
+//
 
+// Fonction d'authentification
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js'
+
+// Fonctions Firestore
+import { 
+  getFirestore, 
+  collection, 
+  onSnapshot, 
+  query,
+  where
+} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js'
+
+//
+//
+//
 
 import HomeView from '../views/HomeView.vue'
 
@@ -33,34 +51,74 @@ const router = createRouter({
 
 
       { path: '/', name: 'ConnexionView', component: ConnexionView },
-      { path: '/home', name: 'homeView', component: HomeView },
+      { path: '/home', name: 'homeView', component: HomeView, beforeEnter:guard  },
 
 
 
-    { path: '/create_quest', name: 'QuestCreateView', component: QuestCreateView, },
-    { path: '/custom_quest/:id', name: 'QuestModifView', component: QuestModifView, },
-    { path: '/delete_quest/:id', name: 'QuestDeleteView', component: QuestDeleteView, },
+    { path: '/create_quest', name: 'QuestCreateView', component: QuestCreateView, beforeEnter:guard  },
+    { path: '/custom_quest/:id', name: 'QuestModifView', component: QuestModifView, beforeEnter:guard  },
+    { path: '/delete_quest/:id', name: 'QuestDeleteView', component: QuestDeleteView, beforeEnter:guard  },
 
 
-    { path: '/history', name: 'HistListeView', component: HistListeView, },
+    { path: '/history', name: 'HistListeView', component: HistListeView, beforeEnter:guard },
 
-    { path: '/agenda', name: 'AgendaView', component: AgendaView, },
-    { path: '/avatar', name: 'AvatarView', component: AvatarView, },
-    { path: '/custom_avatar', name: 'CustomAvatarView', component: CustomAvatarView },
+    { path: '/agenda', name: 'AgendaView', component: AgendaView, beforeEnter:guard  },
+    { path: '/avatar', name: 'AvatarView', component: AvatarView, beforeEnter:guard  },
+    { path: '/custom_avatar', name: 'CustomAvatarView', component: CustomAvatarView, beforeEnter:guard  },
 
-    { path: '/listecat', name: 'ListecatView', component: ListecatView, },
+    { path: '/listecat', name: 'ListecatView', component: ListecatView, beforeEnter:guard  },
 
-    { path: '/contact', name: 'ContactView', component: ContactView, },
-    { path: '/mentionslegales', name: 'MentionslegalesView', component: MentionslegalesView, },
-    { path: '/about', name: 'AboutView', component: AboutView, },
-    { path: '/apropos', name: 'AproposView', component: AproposView, },
-    { path: '/:pathMatch(.*)', name: 'View404', component: View404 },
+    { path: '/contact', name: 'ContactView', component: ContactView, beforeEnter:guard  },
+    { path: '/mentionslegales', name: 'MentionslegalesView', component: MentionslegalesView, beforeEnter:guard  },
+    { path: '/about', name: 'AboutView', component: AboutView, beforeEnter:guard  },
+    { path: '/apropos', name: 'AproposView', component: AproposView, beforeEnter:guard  },
+    { path: '/:pathMatch(.*)', name: 'View404', component: View404, beforeEnter:guard  },
 
-    { path: '/gds', name: 'GuideStyle', component: GuideStyle },
+    { path: '/gds', name: 'GuideStyle', component: GuideStyle, beforeEnter:guard  },
 
-    { path: '/social', name: 'Socialmedia', component: Socialmedia },
+    { path: '/social', name: 'Socialmedia', component: Socialmedia, beforeEnter:guard  },
 
   ]
 })
+
+// On créé un guard : Observateur (fonction) permettant de savoir si un utilisateur
+// a le droit d'utiliser une route
+// paramètres : to : d'où il vient, from où il veut aller, next où il doit aller après contrôle
+function guard(to, from, next) {
+  // recherche utilisateur connecté
+  getAuth().onAuthStateChanged(function(user) {
+    if(user) {
+      // User connecté
+//      console.log('router OK => user ', user);
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document participant
+      const dbUsers = collection(firestore, "users");
+      // Recherche du user par son uid
+      const q = query(dbUsers, where("uid", "==", user.uid));
+      onSnapshot(q, (snapshot) => {
+          let userInfo = snapshot.docs.map(doc => ( {id:doc.id, ...doc.data()}));
+          // userInfo étant un tableau, on récupère
+          // ses informations dans la 1° cellule du tableau : 0
+          let accesPages=null;
+          accesPages=userInfo[0];
+          if(accesPages != null){
+
+            next(to.params.name);
+            return;
+          }else{
+            // Utilisateur non administrateur, renvoi sur accueil
+            alert("Vous n'êtes pas connecté !");
+            next({name: "ConnexionView"});
+            return;
+          }
+      })
+    }else {
+      // Utilisateur non connecté, renvoi sur accueil
+//      console.log('router NOK => user ', user);
+      next({name: "ConnexionView"});
+    }
+  });
+}
 
 export default router
